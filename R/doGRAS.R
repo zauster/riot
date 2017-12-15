d3bc7c93e8efadfac867099a9cc3b1f1d0249cf9
zauster <oliver_reiter@gmx.at>
2017-12-15 23:59:58 +0100

    Documentation update

--- R/doGRAS.R
+++ R/doGRAS.R
@@ -15,10 +15,10 @@
 #' @references Junius T. and J. Oosterhaven (2003), The solution of
 #'     updating or regionalizing a matrix with both positive and
 #'     negative entries, Economic Systems Research, 15, pp. 87-96.
-#' 
+#'
 #'     Lenzen M., R. Wood and B. Gallego (2007), Some comments on the
 #'     GRAS method, Economic Systems Research, 19, pp. 461-465.
-#' 
+#'
 #'     Temurshoev, U., R.E. Miller and M.C. Bouwmeester (2013), A note
 #'     on the GRAS method, Economic Systems Research, 25, pp. 361-367.
 #' @keywords gras, matrix updating
@@ -35,92 +35,98 @@ doGRAS <- function(A, u, v,
                    max.iter = 10000,
                    verbose = FALSE) {
 
-    ## TODO:
-    ## check for correct dimensions
-    ## conversion to vectors/matrices
-    ## what about NAs?
+  ## TODO:
+  ## check for correct dimensions
+  ## conversion to vectors/matrices
+  ## what about NAs?
 
-    ## get col/row numbers
-    m <- nrow(A)
-    n <- ncol(A)
+  ## get col/row numbers
+  nrowsA <- nrow(A)
+  ncolsA <- ncol(A)
 
-    if(n != length(v)) {
-        stop(paste0("Unequal number of columns! ncol(A) = ", n, " and length(v) = ", length(v),
-                    " should be equal!"))
-    }
-    if(m != length(u)) {
-        stop(paste0("Unequal number of rows! nrow(A) = ", m, " and length(u) = ", length(u),
-                    " should be equal!"))
-    }
+  if(ncolsA != length(v)) {
+    stop(paste0("Unequal number of columns! ncol(A) = ", ncolsA,
+                " and length(v) = ", length(v),
+                " should be equal!"))
+  }
+  if(nrowsA != length(u)) {
+    stop(paste0("Unequal number of rows! nrow(A) = ", nrowsA,
+                " and length(u) = ", length(u),
+                " should be equal!"))
+  }
 
-    ## separate A into positive (P) and negative (N) matrizes
-    P <- A
-    N <- abs(A)
-    N[A >= 0] <- 0 ## set positive numbers to 0
-    P[A < 0] <- 0  ## set negative numbers to 0
+  ## separate A into positive (P) and negative (N) matrizes
+  P <- A
+  N <- abs(A)
+  N[A >= 0] <- 0 ## set positive numbers to 0
+  P[A < 0] <- 0  ## set negative numbers to 0
 
-    ## initialize r
-    r <- rep(1, m)
-    s <- rep(1, n)
-    error <- 1
-    iter <- 1
+  ## initialize r
+  r <- rep(1, nrowsA)
+  s <- rep(1, ncolsA)
+  error <- 1
+  iter <- 1
 
-    while((error > epsilon) & (iter <= max.iter)) {
+  while((error > epsilon) & (iter <= max.iter)) {
 
-        s.old <- s
+    s.old <- s
 
-        ##
-        ## calculate multiplicator s
-        ## 
-        pj.r <- t(P) %*% r
-        nj.r <- t(N) %*% sinv(r)
+    ##
+    ## calculate multiplicator s
+    ##
+    pj.r <- t(P) %*% r
+    nj.r <- t(N) %*% sinv(r)
 
-        s <- sinv(2 * pj.r) * (v + sqrt(v^2 + 4 * pj.r * nj.r))
-        s.alt <- -sinv(v) * nj.r
-        s[s == 0] <- s.alt[s == 0]
+    s <- sinv(2 * pj.r) * (v + sqrt(v^2 + 4 * pj.r * nj.r))
+    s.alt <- -sinv(v) * nj.r
+    s[s == 0] <- s.alt[s == 0]
 
-        ##
-        ## calculate multiplicator r
-        ##
-        pi.s <- P %*% s
-        ni.s <- N %*% sinv(s)
+    ##
+    ## calculate multiplicator r
+    ##
+    pi.s <- P %*% s
+    ni.s <- N %*% sinv(s)
 
-        r <- sinv(2 * pi.s) * (u + sqrt(u^2 + 4 * pi.s * ni.s))
-        r.alt <- -sinv(u) * ni.s
-        r[r == 0] <- r.alt[r == 0]
+    r <- sinv(2 * pi.s) * (u + sqrt(u^2 + 4 * pi.s * ni.s))
+    r.alt <- -sinv(u) * ni.s
+    r[r == 0] <- r.alt[r == 0]
 
-        ##
-        ## calculate the remaining error
-        diff <- abs(s.old - s)
-        error <- max(diff)
-        error.pos <- which.max(diff)
-
-        if(verbose) {
-            message("iter: ", iter, "  -> error: ", error, " at ", error.pos)
-            }
-        
-        iter <- iter + 1
-    }
-
-    if(iter >= max.iter) {
-        warning(paste0(" => Stopped after ", max.iter, " iterations. Optimal solution is NOT guaranteed!"))
-    }
+    ##
+    ## calculate the remaining error
+    diff <- abs(s.old - s)
+    error <- max(diff)
+    error.pos <- which.max(diff)
 
     if(verbose) {
-        message(" => Iterations needed: ", iter - 1)
-        message(" => Resulting error: ", error, " at ", error.pos)
-        }
+      message("iter: ", iter, "  -> error: ", error, " at ", error.pos)
+    }
 
-    r <- as.vector(r)
-    s <- as.vector(s)
+    iter <- iter + 1
+  }
 
-    ## X.1 <- diag(r) %*% P %*% diag(s) - diag(sinv(r)) %*% N %*% diag(sinv(s))
-    X <- sweep(r * P, 2, s, "*") - sweep(sinv(r) * N, 2, sinv(s), "*")
-    
-    return(X)
+  if(iter >= max.iter) {
+    warning(paste0(" => Stopped after ", max.iter, " iterations. Optimal solution is NOT guaranteed!"))
+  }
+
+  if(verbose) {
+    message(" => Iterations needed: ", iter - 1)
+    message(" => Resulting error: ", error, " at ", error.pos)
+  }
+
+  r <- as.vector(r)
+  s <- as.vector(s)
+
+
+  r
+  s
+
+  ## X.1 <- diag(r) %*% P %*% diag(s) - diag(sinv(r)) %*% N %*% diag(sinv(s))
+  X <- sweep(r * P, 2, s, "*") - sweep(sinv(r) * N, 2, sinv(s), "*")
+
+  return(X)
 }
 
-   
+
 #' Do the GRAS algorithm for a "long" data.table
 #'
 #' This function calculates an updated "long" data.table, based on a data.table dt,
@@ -137,10 +143,10 @@ doGRAS <- function(A, u, v,
 #' @references Junius T. and J. Oosterhaven (2003), The solution of
 #'     updating or regionalizing a matrix with both positive and
 #'     negative entries, Economic Systems Research, 15, pp. 87-96.
-#' 
+#'
 #'     Lenzen M., R. Wood and B. Gallego (2007), Some comments on the
 #'     GRAS method, Economic Systems Research, 19, pp. 461-465.
-#' 
+#'
 #'     Temurshoev, U., R.E. Miller and M.C. Bouwmeester (2013), A note
 #'     on the GRAS method, Economic Systems Research, 25, pp. 361-367.
 #' @keywords gras, matrix updating
@@ -149,26 +155,26 @@ doGRAS <- function(A, u, v,
 doGRAS.long <- function(dt, rowcol, colcol,
                         rowsum, colsum, ...) {
 
-    mat.wide <- dcast.data.table(dt, get(rowcol) ~ get(colcol), fill = 0)
+  mat.wide <- dcast.data.table(dt, get(rowcol) ~ get(colcol), fill = 0)
 
-    ## save the rowvalues for later on
-    ## rowvalues <- mat.wide[, 1, with = FALSE] # can't get it to work with the name of the column...
-    rowvalues <- mat.wide[, rowcol]
+  ## save the rowvalues for later on
+  ## rowvalues <- mat.wide[, 1, with = FALSE] # can't get it to work with the name of the column...
+  rowvalues <- mat.wide[, rowcol]
 
-    ## extract the matrix
-    mat <- as.matrix(mat.wide[, 2:ncol(mat.wide), with = FALSE])
-    
-    ## do the RASing
-    res <- doGRAS(mat, rowsum, colsum, ...)
+  ## extract the matrix
+  mat <- as.matrix(mat.wide[, 2:ncol(mat.wide), with = FALSE])
 
-    ## feed the matrix back into a data.table
-    res <- data.table(prod.na = rowvalues, 
-                      res)
-    
-    ## and convert it back to a long data.table
-    dt <- melt.data.table(res, id.vars = "prod.na",
-                          variable.name = "induse",
-                          variable.factor = FALSE)
-    
-    dt
+  ## do the RASing
+  res <- doGRAS(mat, rowsum, colsum, ...)
+
+  ## feed the matrix back into a data.table
+  res <- data.table(prod.na = rowvalues,
+                    res)
+
+  ## and convert it back to a long data.table
+  dt <- melt.data.table(res, id.vars = "prod.na",
+                        variable.name = "induse",
+                        variable.factor = FALSE)
+
+  dt
 }
---
+++ R/riot.R
@@ -0,0 +1,11 @@
+#' riot
+#'
+#' R Input-Output Tools contains utility functions for input-output database related stuff. Input-Output as the matrizes Leontief invented.
+#'
+#' @docType package
+#' @author Oliver Reiter
+#' @import Rcpp data.table
+#' @importFrom Rcpp evalCpp
+#' @useDynLib riot
+#' @name riot
+NULL
